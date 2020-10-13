ac390d967a998939e83a5fc2410819bc880610b9
Craig Lane <craig.lane@pubnub.com>
2020-10-13 13:10:26 -0700

    Fixing lint errors

--- Tests/PubNubTests/Mocking/MockURLSession.swift
+++ Tests/PubNubTests/Mocking/MockURLSession.swift
@@ -109,7 +109,7 @@ class MockURLSessionUploadTask: URLSessionUploadTask {
   override var countOfBytesExpectedToReceive: Int64 {
     return 0
   }
-  
+
   override var progress: Progress {
     return Progress(totalUnitCount: 0)
   }
@@ -188,7 +188,7 @@ class MockURLSessionDownloadTask: URLSessionDownloadTask {
   override var countOfBytesExpectedToReceive: Int64 {
     return 0
   }
-  
+
   override var progress: Progress {
     return Progress(totalUnitCount: 0)
   }

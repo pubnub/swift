4b2596e238904804927d952d3aca0cc441f329e1
jguz-pubnub <102806147+jguz-pubnub@users.noreply.github.com>
2023-05-18 15:46:22 +0200

    Added an additional test case for excluded_devices (#135)
    Added an additional test case for excluded_devices
    PubNub SDK 6.0.5 release

--- Tests/PubNubTests/Push/PubNubPushTargetTests.swift
+++ Tests/PubNubTests/Push/PubNubPushTargetTests.swift
@@ -30,37 +30,42 @@ import XCTest
 
 class PubNubPushTargetTests: XCTestCase {
   func test_ExcludedDeviceTokensAreUppercased() {
-    let excludedDevices = [
-      "fafb34563ce",
-      "7654eghjkl"
-    ]
-    let pushMessage = PubNubPushMessage(
-      apns: PubNubAPNSPayload(
-        aps: APSPayload(
-          alert: .object(.init(title: "Message")),
-          badge: 2,
-          sound: .string("default")
-        ),
-        pubnub: [.init(
-          targets: [.init(topic: "com.pubnub.swift", environment: .production, excludedDevices: excludedDevices)],
-          collapseID: "SwiftSDK"
-        )],
-        payload: "Push Message from PubNub Swift SDK"
-      )
-    )
+    let excludedDevices = ["fafb3456", "7654egh"]
+    let message = testPushMessage(with: .init(topic: "com.pubnub", environment: .production, excludedDevices: excludedDevices))
     
-    let encodedData = try? Constant.jsonEncoder.encode(pushMessage)
+    let encodedData = try? Constant.jsonEncoder.encode(message)
+    let encodedStr = String(data: encodedData ?? Data(), encoding: .utf8) ?? ""
+    let expectedExclDevices = excludedDevices.map { $0.uppercased() }.jsonStringify ?? ""
+    
+    XCTAssertEqual(retrieveExcludedDevicesValue(from: encodedStr), expectedExclDevices)
+  }
+  
+  func test_ExcludedDeviceTokensAreNotSentIfNotProvided() {
+    let message = testPushMessage(with: .init(topic: "com.pubnub", environment: .production))
+    let encodedData = try? Constant.jsonEncoder.encode(message)
     let encodedStr = String(data: encodedData ?? Data(), encoding: .utf8) ?? ""
     
-    // Retrieves a value for the "excluded_devices" key:
-    let regex = try! NSRegularExpression(pattern: "(?<=\"excluded_devices\":)(.*?)](=?)")
-    let regexMatch = regex.matches(in: encodedStr, range: NSRange(location: 0, length: encodedStr.count))[0]
-    let startIdx = encodedStr.index(encodedStr.startIndex, offsetBy: regexMatch.range.location)
-    let endIdx = encodedStr.index(startIdx, offsetBy: regexMatch.range.length - 1)
-      
-    XCTAssertEqual(
-      String(encodedStr[startIdx...endIdx]),
-      excludedDevices.map { $0.uppercased() }.jsonStringify ?? ""
-    )
+    XCTAssertEqual(retrieveExcludedDevicesValue(from: encodedStr), nil)
+  }
+}
+
+fileprivate func testPushMessage(with target: PubNubPushTarget) -> PubNubAPNSPayload {
+  PubNubAPNSPayload(
+    aps: APSPayload(alert: .object(.init(title: "Apple Message")), badge: 1, sound: .string("default")),
+    pubnub: [.init(targets: [target], collapseID: "SwiftSDK")],
+    payload: "Push Message from PubNub Swift SDK"
+  )
+}
+
+fileprivate func retrieveExcludedDevicesValue(from string: String) -> String? {
+  let regex = try! NSRegularExpression(pattern: "(?<=\"excluded_devices\":)(.*?)](=?)")
+  let regexMatch = regex.matches(in: string, range: NSRange(location: 0, length: string.count)).first
+  
+  if let regexMatch = regexMatch {
+    let startIdx = string.index(string.startIndex, offsetBy: regexMatch.range.location)
+    let endIdx = string.index(startIdx, offsetBy: regexMatch.range.length - 1)
+    return String(string[startIdx...endIdx])
+  } else {
+    return nil
   }
 }

a8e90aaad4ec8e49e53d4763e1d628bb32682f75
jguz-pubnub <jakub.guz@pubnub.com>
2024-04-16 15:20:55 +0200

    Fixing an error regarding embedding PrivacyInfo.xcprivacy

--- Package.swift
+++ Package.swift
@@ -46,7 +46,8 @@ let package = Package(
     // Targets can depend on other targets in this package, and on products in packages which this package depends on.
     .target(
       name: "PubNub",
-      path: "Sources/PubNub"
+      path: "Sources/PubNub",
+      resources: [.copy("PrivacyInfo.xcprivacy")]
     ),
     .target(
       name: "PubNubUser",

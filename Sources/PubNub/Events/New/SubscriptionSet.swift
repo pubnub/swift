91a69fb846a4d0aa94f712aafb8fb644d14fff34
jguz-pubnub <jakub.guz@pubnub.com>
2024-06-19 15:09:43 +0200

    Changed method signature for removing event listener

--- Sources/PubNub/Events/New/SubscriptionSet.swift
+++ Sources/PubNub/Events/New/SubscriptionSet.swift
@@ -152,8 +152,8 @@ public final class SubscriptionSet: EventListenerInterface, SubscriptionDisposab
   }
 
   /// Removes subscription listener
-  public func removeEventListener(_ listener: EventListener) {
-    listenersContainer.removeEventListener(with: listener.uuid)
+  public func removeEventListener(with uuid: UUID) {
+    listenersContainer.removeEventListener(with: uuid)
   }
 
   /// Removes all event listeners

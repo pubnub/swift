7c946ee42beeb5fc7acaf722bf969ba357047440
jguz-pubnub <jakub.guz@pubnub.com>
2024-07-18 11:39:22 +0200

    Additional constructor for PubNubSubscriptionSetObjC

--- Sources/PubNub/KMM/Wrappers/PubNubSubscriptionObjC.swift
+++ Sources/PubNub/KMM/Wrappers/PubNubSubscriptionObjC.swift
@@ -124,6 +124,11 @@ public class PubNubSubscriptionSetObjC: NSObject {
   init(subscriptionSet: SubscriptionSet) {
     self.subscriptionSet = subscriptionSet
   }
+  
+  @objc
+  public init(subscriptions: [PubNubSubscriptionObjC]) {
+    self.subscriptionSet = SubscriptionSet(subscriptions: subscriptions.map { $0.subscription })
+  }
 
   @objc
   public func dispose() {

18cb494326fbed6c02a318d12855b89b6d6c2d22
jguz-pubnub <jakub.guz@pubnub.com>
2025-05-28 15:11:45 +0200

    Work in progress

---
+++ Snippets/Configuration/01-crypto-module.swift
@@ -0,0 +1,32 @@
+import PubNubSDK
+
+func aesCbcCryptoModuleExample() {
+  // snippet.crypto_module
+  // Provides the recommended encryption mechanism (256-bit AES-CBC) introduced
+  // from version 6.1.0. Also supports decryption of data encrypted using the legacy
+  // method used prior to version 6.1.0, ensuring backward compatibility:
+  let pubnub = PubNub(
+    configuration: PubNubConfiguration(
+      publishKey: "yourPublishKey",
+      subscribeKey: "yourSubscribeKey",
+      userId: "yourUserId"
+      cryptoModule: CryptoModule.aesCbcCryptoModule(with: "pubnubenigma")
+    )
+  )
+  // snippet.end
+}
+
+func legacyCryptoModuleExample() {
+  // snippet.legacy_crypto_module
+  // Uses a legacy encryption mechanism (128-bit cipher key entropy) that is no longer recommended.
+  // Supports only decryption of data encrypted with the legacy method used prior to version 6.1.0.
+  let pubnub = PubNub(
+    configuration: PubNubConfiguration(
+      publishKey: "yourPublishKey",
+      subscribeKey: "yourSubscribeKey",
+      userId: "yourUserId"
+      cryptoModule: CryptoModule.aesCbcCryptoModule(with: "pubnubenigma")
+    )
+  )
+  // snippet.end
+}

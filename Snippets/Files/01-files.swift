c068b08fec6692175a000aa8ffca1594d596e904
jguz-pubnub <jakub.guz@pubnub.com>
2025-06-03 19:38:22 +0200

    Work in progress (fix for Files)

--- Snippets/Files/01-files.swift
+++ Snippets/Files/01-files.swift
@@ -69,11 +69,11 @@ let downloadURL = try? pubnub.generateFileDownloadURL(
 // snippet.end
 
 // snippet.download-file
+// Replace `fileURL:` with your destination URL
 let requestFile = PubNubLocalFileBase(
-  fileURL: NSTemporaryDirectory().appending(UUID().uuidString),
   channel: "my_channel",
   fileId: "fileId-from-PubNub",
-  remoteFilename: "filename-from-PubNub"
+  fileURL: URL(fileURLWithPath: "your/download/url.png")
 )
 
 pubnub.download(file: requestFile, toFileURL: requestFile.fileURL) { fileTask in
@@ -82,11 +82,122 @@ pubnub.download(file: requestFile, toFileURL: requestFile.fileURL) { fileTask in
   print("You can use `fileTask.progress` to populate a  `UIProgressView`/`ProgressView`")
 } completion: { result in
   switch result {
-  case let .success(task, newFile):
+  case let .success((task, newFile)):
     print("The file task \(task.taskIdentifier) downloaded successfully to \(newFile.fileURL), which might be different than \(requestFile.fileURL)")
     print("This also might mean that \(newFile.filename) could be different from \(newFile.remoteFilename)")
   case let .failure(error):
     print("An error occurred while downloading the file: \(error.localizedDescription)")
+  }
+}
+// snippet.end
+
+// snippet.download-file-resume
+// Assuming this is the portion of the file that was downloaded prior to the interruption.
+// See above documentation on ways you can obtain this data
+let resumeData = Data()
+
+// Replace `fileURL:` with your destination URL
+let fileToResume = PubNubLocalFileBase(
+  channel: "my_channel",
+  fileId: "fileId-from-PubNub",
+  fileURL: URL(fileURLWithPath: "your/download/url.png")
+)
+
+pubnub.download(
+  file: fileToResume,
+  toFileURL: fileToResume.fileURL,
+  resumeData: resumeData
+) { (fileTask: HTTPFileDownloadTask) in
+  print("The task \(fileTask.urlSessionTask.taskIdentifier) has started downloading; no need to call `resume()`")
+  print("If needed, the `URLSessionUploadTask` can be accessed with `fileTask.urlSessionTask`")
+  print("You can use `fileTask.progress` to populate a  `UIProgressView`/`ProgressView` ")
+} completion: { result in
+  switch result {
+  case let .success((task, newFile)):
+    print("The file task \(task.taskIdentifier) downloaded successfully to \(newFile.fileURL), which might be different than \(fileToResume.fileURL)")
+    print("This also might mean that \(newFile.filename) could be different from \(newFile.remoteFilename)")
+  case let .failure(error):
+    print("An error occurred while downloading the file: \(error.localizedDescription)")
+  }
+}
+// snippet.end
+
+// snippet.download-file-custom-url-session
+let pubNubWithCustomFileURLSession = PubNub(
+  configuration: PubNubConfiguration(
+    publishKey: "demo",
+    subscribeKey: "demo",
+    userId: "myUniqueUserId"
+  ),
+  fileSession: URLSession(
+    configuration: .init(),
+    delegate: FileSessionManager(),
+    delegateQueue: nil
+  )
+)
+
+// Replace `fileURL:` with your destination URL
+let fileToDownload = PubNubLocalFileBase(
+  channel: "my_channel",
+  fileId: "fileId-from-PubNub",
+  fileURL: URL(fileURLWithPath: "your/download/url.png")
+)
+
+pubNubWithCustomFileURLSession.download(
+  file: fileToDownload,
+  toFileURL: fileToDownload.fileURL
+) { (fileTask: HTTPFileDownloadTask) in
+  print("The task \(fileTask.urlSessionTask.taskIdentifier) has started downloading; no need to call `resume()`")
+  print("If needed, the `URLSessionUploadTask` can be accessed with `fileTask.urlSessionTask`")
+  print("You can use `fileTask.progress` to populate a  `UIProgressView`/`ProgressView` ")
+} completion: { result in
+  switch result {
+  case let .success((task, newFile)):
+    print("The file task \(task.taskIdentifier) downloaded successfully to \(newFile.fileURL), which might be different than \(fileToDownload.fileURL)")
+    print("This also might mean that \(newFile.filename) could be different from \(newFile.remoteFilename)")
+  case let .failure(error):
+    print("An error occurred while downloading the file: \(error.localizedDescription)")
+  }
+}
+// snippet.end
+
+// snippet.remove-file
+pubnub.remove(
+  fileId: "id-of-a-file",
+  filename: "example.png",
+  channel: "my_channel"
+) { result in
+  switch result {
+  case let .success(response):
+    print("The file whose ID is \(response.fileId) was removed from \(response.channel)")
+  case let .failure(error):
+    print("An error occurred while removing the file: \(error.localizedDescription)")
+  }
+}
+// snippet.end
+
+// snippet.publish-file-request
+// Replace `fileURL:` with your destination URL
+let someFile = PubNubLocalFileBase(
+  channel: "my_channel",
+  fileId: "fileId-from-PubNub",
+  fileURL: URL(fileURLWithPath: "path/to/example.png")
+)
+
+let publishRequest = PubNub.PublishFileRequest(
+  additionalMessage: "Some additional message",
+  customMessageType: "customMessageType"
+)
+
+pubnub.publish(
+  file: file,
+  request: publishRequest
+) { result in
+  switch result {
+  case let .success(timetoken):
+    print("File Successfully Published at: \(timetoken)")
+  case let .failure(error):
+    print("Error publishing file: \(error.localizedDescription)")
   }
 }
 // snippet.end

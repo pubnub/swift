b4dca2700becf772853ad6cd8e62a913a64dba84
Jakub Guz <jakub.guz@pubnub.com>
2025-08-07 12:14:33 +0000

    PubNub SDK 9.3.3 release.

--- Sources/PubNub/Helpers/Constants.swift
+++ Sources/PubNub/Helpers/Constants.swift
@@ -57,7 +57,7 @@ public enum Constant {
 
   static let pubnubSwiftSDKName: String = "PubNubSwift"
 
-  static let pubnubSwiftSDKVersion: String = "9.3.2"
+  static let pubnubSwiftSDKVersion: String = "9.3.3"
 
   static let appBundleId: String = {
     if let info = Bundle.main.infoDictionary,

44520a1a93af58386570e1caaf75823351132bf0
jguz-pubnub <jakub.guz@pubnub.com>
2025-11-06 14:24:59 +0100

    Remove deprecated MPNS (Microsoft Push Notification Service) support

--- Sources/PubNub/Models/PubNubPushMessage.swift
+++ Sources/PubNub/Models/PubNubPushMessage.swift
@@ -15,8 +15,7 @@ public struct PubNubPushMessage: JSONCodable {
   public let apns: PubNubAPNSPayload?
   /// The payload delivered via Firebase Cloud Messaging Service (FCM)
   public let fcm: PubNubFCMPayload?
-  /// The payload delivered via Microsoft Push Notification Service (MPNS)
-  public let mpns: JSONCodable?
+
   /// Additional message payload sent outside of the push notification
   ///
   /// In order to guarantee valid JSON any scalar values will be assigned to the `data` key.
@@ -26,19 +25,16 @@ public struct PubNubPushMessage: JSONCodable {
   enum CodingKeys: String, CodingKey {
     case apns = "pn_apns"
     case fcm = "pn_fcm"
-    case mpns = "pn_mpns"
     case additionalMessage = "data"
   }
 
   public init(
     apns: PubNubAPNSPayload? = nil,
     fcm: PubNubFCMPayload? = nil,
-    mpns: JSONCodable? = nil,
     additional message: JSONCodable? = nil
   ) {
     self.apns = apns
     self.fcm = fcm
-    self.mpns = mpns
     additionalMessage = message
   }
 
@@ -46,7 +42,6 @@ public struct PubNubPushMessage: JSONCodable {
     let container = try decoder.container(keyedBy: CodingKeys.self)
     apns = try container.decodeIfPresent(PubNubAPNSPayload.self, forKey: .apns)
     fcm = try container.decodeIfPresent(PubNubFCMPayload.self, forKey: .fcm)
-    mpns = try container.decodeIfPresent(AnyJSON.self, forKey: .mpns)
   }
 
   public func encode(to encoder: Encoder) throws {
@@ -57,7 +52,6 @@ public struct PubNubPushMessage: JSONCodable {
     var container = encoder.container(keyedBy: CodingKeys.self)
     try container.encodeIfPresent(apns, forKey: .apns)
     try container.encodeIfPresent(fcm, forKey: .fcm)
-    try container.encodeIfPresent(mpns?.codableValue, forKey: .mpns)
 
     if additionalMessage?.codableValue.isScalar ?? true {
       try container.encodeIfPresent(additionalMessage?.codableValue, forKey: .additionalMessage)

cddc5c38d44b9218c8a6ab80dcc593e91abf331e
jguz-pubnub <102806147+jguz-pubnub@users.noreply.github.com>
2026-09-03 16:30:52 +0200

    Add APNs push type to PubNubPushConfig (#262)

--- Sources/PubNub/Helpers/Constants.swift
+++ Sources/PubNub/Helpers/Constants.swift
@@ -57,7 +57,7 @@ public enum Constant {
 
   static let pubnubSwiftSDKName: String = "PubNubSwift"
 
-  static let pubnubSwiftSDKVersion: String = "10.1.8"
+  static let pubnubSwiftSDKVersion: String = "10.2.0"
 
   static let appBundleId: String = {
     if let info = Bundle.main.infoDictionary,
